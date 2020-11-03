a9e58c027190604008bdd4495cc503c914c45ca6
adenosinetp10 <63637385+adenosinetp10@users.noreply.github.com>
2020-11-03 22:20:16 +0530

    Update RSSscrap.py

--- RSSscrap.py
+++ RSSscrap.py
@@ -12,56 +12,59 @@ CHANNEL_ID = -1001340269031
 
 
 def start(update, context):
-    
-    if update.effective_chat.id != CHANNEL_ID:
-        pass
-    else:
-        nyaa_id1=""
-        while True:
-            time.sleep(5)
-            r = requests.get(url)
-            soup = BeautifulSoup(r.content,features='xml')
-            all_title = soup.findAll("title")
-            all_download = soup.findAll("link")
-            all_view = soup.findAll("guid",{"isPermaLink":"true"})
-            all_hash = soup.findAll("nyaa:infoHash")
-            all_date = soup.findAll("pubDate")
-            all_size = soup.findAll("nyaa:size")
-            all_category = soup.findAll("nyaa:category")
-            spec_title = all_title[1].text
-            spec_download = all_download[2].text
-            spec_view = all_view[0].text
-            spec_date = all_date[0].text
-            spec_size = all_size[0].text
-            spec_category = all_category[0].text
-            spec_hash = all_hash[0].text
-            nyaa_id = nyaa_id1
-            if (nyaa_id!=spec_view):
-                nyaa_id = spec_view
-                nyaa_id1 = nyaa_id
-                keyboard = [
-                [
-                InlineKeyboardButton("More Info",url = str(spec_view)),
-                InlineKeyboardButton("Download\nTorrent",url = str(spec_download))]
-                            ]
-                reply_markup1 = InlineKeyboardMarkup(keyboard)
+    if update.effective_message.text == 'start':
+        if update.effective_chat.id != CHANNEL_ID:
+            pass
+        else:
+            nyaa_id1=""
+            while True:
+                time.sleep(5)
+                r = requests.get(url)
+                soup = BeautifulSoup(r.content,features='xml')
+                all_title = soup.findAll("title")
+                all_download = soup.findAll("link")
+                all_view = soup.findAll("guid",{"isPermaLink":"true"})
+                all_hash = soup.findAll("nyaa:infoHash")
+                all_date = soup.findAll("pubDate")
+                all_size = soup.findAll("nyaa:size")
+                all_category = soup.findAll("nyaa:category")
+                spec_title = all_title[1].text
+                spec_download = all_download[2].text
+                spec_view = all_view[0].text
+                spec_date = all_date[0].text
+                spec_size = all_size[0].text
+                spec_category = all_category[0].text
+                spec_hash = all_hash[0].text
+                nyaa_id = nyaa_id1
+                if (nyaa_id!=spec_view):
+                    nyaa_id = spec_view
+                    nyaa_id1 = nyaa_id
+                    keyboard = [
+                    [
+                    InlineKeyboardButton("More Info",url = str(spec_view)),
+                    InlineKeyboardButton("Download\nTorrent",url = str(spec_download))]
+                                ]
+                    reply_markup1 = InlineKeyboardMarkup(keyboard)
 
-                update.effective_message.reply_text("<b>Name : <pre>%s</pre></b>\n<b>Category :</b> <pre>%s</pre>\n<b>Size :</b> <pre>%s</pre>\n<b>Publish Date :</b> <pre>%s</pre>\n<b>Magnet Link :</b> <pre>magnet:?xt=urn:btih:%s</pre>"%(spec_title, spec_category, spec_size, spec_date.replace("-0000","GMT"),spec_hash),parse_mode = 'HTML', reply_markup = reply_markup1,quote = False)
-            return
+                    update.effective_message.reply_text("<b>Name : <pre>%s</pre></b>\n<b>Category :</b> <pre>%s</pre>\n<b>Size :</b> <pre>%s</pre>\n<b>Publish Date :</b> <pre>%s</pre>\n<b>Magnet Link :</b> <pre>magnet:?xt=urn:btih:%s</pre>"%(spec_title, spec_category, spec_size, spec_date.replace("-0000","GMT"),spec_hash),parse_mode = 'HTML', reply_markup = reply_markup1,quote = False)
+                return
+    else:
+        pass
     
 def test(update,context):
-    
-    if update.effective_chat.id != CHANNEL_ID:
-        update.effective_message.reply_text("un auth",quote=False)
+    if update.effective_message.text == 'test':
+        if update.effective_chat.id != CHANNEL_ID:
+            update.effective_message.reply_text("AUTH : fail",quote=False)
+        else:
+            update.effective_message.reply_text("AUTH : success",quote=False)
     else:
-        update.effective_message.reply_text("Auth",quote=False)
-    
+        pass
 
 def main():
     bot_token=os.environ.get("BOT_TOKEN","")
     updater = Updater(bot_token , use_context = True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start',start,run_async = True))
+    dp.add_handler(MessageHandler(Filters.text,start,run_async = True))
     dp.add_handler(MessageHandler(Filters.text,test,run_async = True))
     updater.start_polling()
     updater.idle()
