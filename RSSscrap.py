d2e07c1e21a3d4d0242e03e980f3a349c83e4b29
adenosinetp10 <63637385+adenosinetp10@users.noreply.github.com>
2020-11-04 11:42:51 +0530

    Update RSSscrap.py

--- RSSscrap.py
+++ RSSscrap.py
@@ -1,4 +1,5 @@
 import requests
+import random
 import time
 import os
 import lxml
@@ -12,44 +13,46 @@ CHANNEL_ID = -1001340269031
 
 
 def start(update:Update, context):
-    if update.effective_message.text == 'start':
-        if update.effective_chat.id != CHANNEL_ID:
-            pass
-        else:
-            nyaa_id1=""
-            while True:
-                time.sleep(5)
-                r = requests.get(url)
-                soup = BeautifulSoup(r.content,features='xml')
-                all_title = soup.findAll("title")
-                all_download = soup.findAll("link")
-                all_view = soup.findAll("guid",{"isPermaLink":"true"})
-                all_hash = soup.findAll("nyaa:infoHash")
-                all_date = soup.findAll("pubDate")
-                all_size = soup.findAll("nyaa:size")
-                all_category = soup.findAll("nyaa:category")
-                spec_title = all_title[1].text
-                spec_download = all_download[2].text
-                spec_view = all_view[0].text
-                spec_date = all_date[0].text
-                spec_size = all_size[0].text
-                spec_category = all_category[0].text
-                spec_hash = all_hash[0].text
-                nyaa_id = nyaa_id1
-                if (nyaa_id!=spec_view):
-                    nyaa_id = spec_view
-                    nyaa_id1 = nyaa_id
-                    keyboard = [
-                    [
-                    InlineKeyboardButton("More Info",url = str(spec_view)),
-                    InlineKeyboardButton("Download\nTorrent",url = str(spec_download))]
-                                ]
-                    reply_markup1 = InlineKeyboardMarkup(keyboard)
+    nyaa_id1 = ""
+    print("going on 0.")
+    while True:
+        print("going on 1.")
+        randomSleep = random.randint(5,10)
+        time.sleep(randomSleep)
+        print(randomSleep)
+        r = requests.get(url)
+        print("going on 2.")
+        soup = BeautifulSoup(r.content,features='xml')
+        all_title = soup.findAll("title")
+        all_download = soup.findAll("link")
+        all_view = soup.findAll("guid",{"isPermaLink":"true"})
+        all_hash = soup.findAll("nyaa:infoHash")
+        all_date = soup.findAll("pubDate")
+        all_size = soup.findAll("nyaa:size")
+        all_category = soup.findAll("nyaa:category")
+        print("going on 3.")
+        spec_title = all_title[1].text
+        spec_download = all_download[2].text
+        spec_view = all_view[0].text
+        spec_date = all_date[0].text
+        spec_size = all_size[0].text
+        spec_category = all_category[0].text
+        spec_hash = all_hash[0].text
+        nyaa_id = nyaa_id1
+        print("going on 4.")
+        if (nyaa_id!=spec_view):
+            nyaa_id = spec_view
+            nyaa_id1 = nyaa_id
+            print("going on 5.")
+            keyboard = [
+            [
+            InlineKeyboardButton("More Info",url = str(spec_view)),
+            InlineKeyboardButton("Download\nTorrent",url = str(spec_download))]
+                        ]
+            reply_markup1 = InlineKeyboardMarkup(keyboard)
 
-                    update.effective_message.reply_text("<b>Name : <pre>%s</pre></b>\n<b>Category :</b> <pre>%s</pre>\n<b>Size :</b> <pre>%s</pre>\n<b>Publish Date :</b> <pre>%s</pre>\n<b>Magnet Link :</b> <pre>magnet:?xt=urn:btih:%s</pre>"%(spec_title, spec_category, spec_size, spec_date.replace("-0000","GMT"),spec_hash),parse_mode = 'HTML', reply_markup = reply_markup1,quote = False)
-                return nyaa_id1
-    else:
-        pass
+            update.effective_message.reply_text("<b>Name : <pre>%s</pre></b>\n<b>Category :</b> <pre>%s</pre>\n<b>Size :</b> <pre>%s</pre>\n<b>Publish Date :</b> <pre>%s</pre>\n<b>Magnet Link :</b> <pre>magnet:?xt=urn:btih:%s</pre>"%(spec_title, spec_category, spec_size, spec_date.replace("-0000","GMT"),spec_hash),parse_mode = 'HTML', reply_markup = reply_markup1,quote = False)
+            print("going on 6.")
     
 def bruh(update:Update,context):
     if update.effective_message.text == 'bruh':
